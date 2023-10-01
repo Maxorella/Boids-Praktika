4d6cb414cb99e2c360a2bce1f196623af67aeb84
Maksim Shagaev <maxorella@mail.ru>
2023-10-01 11:15:10 +0300

    add python file

---
+++ Graphs.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+x=[]
+y=[]
+for i in range(1,34):
+    file1 = open(str(i), 'r')
+    Lines = file1.readlines()
+    x=[]
+    y=[]
+
+    for line in Lines:
+        l = line.split()
+        x.append(float(l[0]))
+        y.append(float(l[1]))
+    print(x)
+    print(y)
+    plt.xlim (-40, 40)
+    plt.ylim (-40, 40)
+
+    plt.plot(x, y,'o', color='black')
+    name = str(i)+'.jpg'
+    plt.savefig(name)
+    plt.cla()
+
+#   plt.savefig('foo.pdf',bbox_inches='tight')
