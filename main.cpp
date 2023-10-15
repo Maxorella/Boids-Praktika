4cbb3c41e22a03b3042db61a24b2f6d839c14343
Alex <95041908+AlexSeliv-22@users.noreply.github.com>
2023-10-15 23:23:38 +0300

    проверка числа аргументов

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include "fieldb.h"
 #include "model.h"
 int main(int argc, char* argv[]){
+  if (argc != 3)
+  {
+    cout << "Error: incorrect number of arguments" << endl;
+    return (-1);
+  }
   string birdfilename = argv[1];
   string carfilename = argv[2];
   FieldBehaviour fb;
