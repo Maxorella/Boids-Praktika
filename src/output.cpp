b7ddb0c4cb569a8abacf0e73d1ffa4b764651996
Maxorella <maxorella@mail.ru>
2024-02-12 23:16:30 +0300

    created src

--- src/output.cpp
+++ src/output.cpp
@@ -10,7 +10,7 @@ void OutputController::createSlice()
     Data dat;
     static int i = 1;
     Vec3Cord v;
-    ofstream outstrm(to_string(i)+".txt");
+    ofstream outstrm("out/"+to_string(i)+".txt");
     if(!outstrm.is_open())
     {
         cout << "Error: file wasn't created." << endl;
